33847c6ec1307cee9afd1edbd0f228542daa3407
Nikesh <nikesh.designer@gmail.com>
2025-06-18 23:28:34 +0530

    OnBoarding Changes From UI

--- src/components/Dashboard/OnBoarding.jsx
+++ src/components/Dashboard/OnBoarding.jsx
@@ -1,76 +1,61 @@
 import React from 'react';
 
-import UserName from '../../assets/Images/user_img.svg';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+import EmployeeProfileDetails from '../EmployeeOnBoarding/EmployeeProfileDetails.jsx';
+
 import Card from 'react-bootstrap/Card';
 
 
 function EmployeeProfileCard() {
-  const employee = {
-    id: 'EMP012547',
-    name: 'Nikesh Balu',
-    designation: 'UI UX Developer',
-    department: 'UI UX Team',
-    joiningDate: '25/07/2023',
-    employmentType: 'Full-Time',
-    manager: 'Muthu Karthikeyan',
-    workLocation: 'Work From Home',
-    status: 'Active',
-    offerLetter: '#', // Link to offer letter (PDF)
-    profilePic: UserName // Replace with actual image URL if needed
-  };
   return (
 
     <Container fluid>
       <Row>
         <Col md={12} lg={12} xl={12} xxl={12}>
-          <Card className='card_purple'>
+          <Card className='card_purple mb-4'>
             <Card.Body>
-              <div className="">
-                <img src={employee.profilePic} alt="Profile" className="rounded-circle me-3" style={{ width: '70px', height: '70px' }} />
-                <div>
-                  <h5 className="mb-0">
-                    {employee.name}{' '}
-                    <span className="badge bg-success ms-2">{employee.status}</span>
-                  </h5>
-                  <p className="mb-0 text-muted">{employee.designation}</p>
-                  <a href="#" className="text-primary fw-bold">
-                    {employee.id}
-                  </a>
-                </div>
-                <div className="">
-                  <small className="">Department</small>
-                  <small>{employee.department}</small>
-                </div>
-                <div className="">
-                  <small className="">Joining Date</small>
-                  <small>{employee.joiningDate}</small>
-                </div>
-                <div className="">
-                  <small className="">Employment Type</small>
-                  <small>{employee.employmentType}</small>
-                </div>
-                <div className="">
-                  <small className="">Manager</small>
-                  <small>{employee.manager}</small>
-                </div>
-                <div className="">
-                  <small className="">Work Location</small>
-                  <small>{employee.workLocation}</small>
-                </div>
-                <div className=''>
-                  <a href={employee.offerLetter} className="text-danger d-flex align-items-center">
-                    Offer Letter
-                  </a>
-                </div>
-              </div>
+              <EmployeeProfileDetails />
             </Card.Body>
           </Card>
+        </Col>
+        <Col md={12} lg={12} xl={12} xxl={12}>
+
+          <Tabs defaultActiveKey="home" transition={false} id="noanim-tab-example" className="mb-3 Secondary_tab">
+            <Tab eventKey="basic_info" title="Basic Info">
+              {/* <BasicInfo/> */}
+            </Tab>
+            <Tab eventKey="contact" title="Contact">
+              {/* <Contact/> */}
+            </Tab>
+            <Tab eventKey="family" title="Family">
+              {/* <Family/> */}
+            </Tab>
+            <Tab eventKey="education" title="Education">
+              {/* <Education/> */}
+            </Tab>
+            <Tab eventKey="work_experience" title="Work Experience">
+              {/* <WorkExperience/> */}
+            </Tab>
+            <Tab eventKey="documents" title="Documents">
+              {/* <Documents/> */}
+            </Tab>
+            <Tab eventKey="benefits" title="Benefits">
+              {/* <Benefits/> */}
+            </Tab>
+            <Tab eventKey="health_record" title="Health Record">
+              {/* <HealthRecord/> */}
+            </Tab>
+            <Tab eventKey="travel" title="Travel">
+              {/* <Travel/> */}
+            </Tab>
+          </Tabs>
         </Col>
       </Row>
     </Container>
---
+++ src/components/EmployeeOnBoarding/EmployeeProfileDetails.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import UserName from '../../assets/Images/user_img.svg';
+import OfferLetter from '../../assets/Images/pdf.svg';
+
+function EmployeeProfileCard() {
+  const employee = {
+    id: 'EMP012547',
+    name: 'Nikesh Balu',
+    designation: 'UI UX Developer',
+    department: 'UI UX Team',
+    joiningDate: '25/07/2023',
+    employmentType: 'Full-Time',
+    manager: 'Muthu Karthikeyan',
+    workLocation: 'Work From Home',
+    status: 'Active',
+    offerLetter: '#', // Link to offer letter (PDF)
+    profilePic: UserName // Replace with actual image URL if needed
+  };
+    return (
+        <div className="employee_profile_container">
+            <div className='employee_profile_pic_content'>
+                <div className='employee_profile_pic'>
+                    <img src={employee.profilePic} alt="Profile" className="rounded-circle" style={{ width: '90px', height: '90px' }} />
+                </div>
+                <div className='employee_profile_content'>
+                    <h5 className="mb-0">
+                        <label>{employee.name}{' '}</label>
+                        <span className="badge">{employee.status}</span>
+                    </h5>
+                    <p className="">{employee.designation}</p>
+                    <a href="#" className="">
+                        {employee.id}
+                    </a>
+                </div>
+            </div>
+            <div className="employee_profile_details">
+                <label>Department</label>
+                <span>{employee.department}</span>
+            </div>
+            <div className="employee_profile_details">
+                <label>Joining Date</label>
+                <span>{employee.joiningDate}</span>
+            </div>
+            <div className="employee_profile_details">
+                <label>Employment Type</label>
+                <span>{employee.employmentType}</span>
+            </div>
+            <div className="employee_profile_details">
+                <label>Manager</label>
+                <span>{employee.manager}</span>
+            </div>
+            <div className="employee_profile_details">
+                <label>Work Location</label>
+                <span>{employee.workLocation}</span>
+            </div>
+            <div className="employee_offer_letter">
+                <a href={employee.offerLetter} className="">
+                    <img src={OfferLetter} alt="Offer Letter" className="offer_letter_icon" />
+                    {' '}
+                    Offer Letter
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default EmployeeProfileCard
