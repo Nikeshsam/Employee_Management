6660bc3c3cd417ea4900661d6b17331518733ec3
NikeshBalu <nikesh.designer@gmail.com>
2025-06-25 11:59:19 +0530

    New UI Changes

--- src/components/EmployeeOnBoarding/WorkExperience.jsx
+++ src/components/EmployeeOnBoarding/WorkExperience.jsx
@@ -17,7 +17,31 @@ const WorkExperience = () => {
         <Form>
           <Row>
             <Col md={12} lg={12} xl={12} xxl={12}>
-
+              <div className='primary_table'>
+                <div className="table_header">
+                  <h5>Work Experience</h5>
+                  <div className="heading_elements">
+                    <ul>
+                      <li><input className='grid_search' type="text" placeholder="Search" /></li>
+                      <li><Button className='primary_form_btn btn_h_35'>Add Work Experience</Button></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='table_body'>
+                  <Table>
+                    <thead>
+                      <tr>
+                        <th>Organization</th>
+                        <th>Location</th>
+                        <th>Job Title</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                  </Table>
+                </div>
+              </div>
             </Col>
           </Row>
         </Form>
@@ -27,7 +51,3 @@ const WorkExperience = () => {
 }
 
 export default WorkExperience
-
-              // <div className="primary_table">
-                
-              // </div>
