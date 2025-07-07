9ad3e4976bbf9bee1de4ccff61aed20248ebf5b0
NikeshBalu <nikesh.designer@gmail.com>
2025-07-07 15:38:48 +0530

    Tab Added

--- src/components/Dashboard/AddEmployee.jsx
+++ src/components/Dashboard/AddEmployee.jsx
@@ -64,46 +64,58 @@ const AddEmployee = () => {
             <Container fluid>
                 <Row>
                     <Col md={12} lg={12} xl={12} xxl={12}>
-                        <PrimaryGird
-                            cardTitle="Employee"
-                            buttonText="Export"
+                        <Tabs defaultActiveKey="total_onboarding" transition={false} id="noanim-tab-example" className="mb-3 Secondary_tab">
+                            <Tab eventKey="total_onboarding" title="Total Onboarding">
+                                <Col md={12} lg={12} xl={12} xxl={12}>
+                                    <PrimaryGird
+                                        cardTitle="Employee"
+                                        buttonText="Export"
 
-                            buttonFilter={Images.GirdFilter}
-                            buttonDelete={Images.GirdDelete}
+                                        buttonFilter={Images.GirdFilter}
+                                        buttonDelete={Images.GirdDelete}
 
-                            onButtonClick={() => console.log('Add clicked')}
-                            onFilterClick={() => console.log('Filter clicked')}
-                            onDeleteClick={() => console.log('Delete clicked')}
+                                        onButtonClick={() => console.log('Add clicked')}
+                                        onFilterClick={() => console.log('Filter clicked')}
+                                        onDeleteClick={() => console.log('Delete clicked')}
 
-                            showAddButton={true}
-                            showFilterButton={true}
-                            showDeleteButton={true}
-                            showFooter={true}
+                                        showAddButton={true}
+                                        showFilterButton={true}
+                                        showDeleteButton={true}
+                                        showFooter={true}
 
-                            buttonClassName='secondary_btn btn_h_35 fs_13 fw_500'
-                            buttonClassIcon='icon_btn'
-                            tableHeaders={[<Form.Check />, 'Employee ID', 'Employee ID', 'Department', 'Status', 'Email', 'Onboarding Status', 'Actions']}
-                        >
-                            {employeeData.map((emp, idx) => (
-                                <tr key={idx}>
-                                    <td><Form.Check /></td>
-                                    <td><a href="#">{emp.id}</a></td>
-                                    <td>{emp.avatar} {emp.name}</td>
-                                    <td>{emp.department}</td>
-                                    <td>
-                                        <span className="badge">
-                                            <i></i> {emp.status}
-                                        </span>
-                                    </td>
-                                    <td>{emp.email}</td>
-                                    <td><OnboardingBadge status={emp.onboarding} /></td>
-                                    <td className='table_action'>
-                                        <Button className="btn_action"><img src={Images.Edit} alt="" /></Button>
-                                        <Button className="btn_action"><img src={Images.Delete} alt="" /></Button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </PrimaryGird>
+                                        buttonClassName='secondary_btn btn_h_35 fs_13 fw_500'
+                                        buttonClassIcon='icon_btn'
+                                        tableHeaders={[<Form.Check />, 'Employee ID', 'Employee ID', 'Department', 'Status', 'Email', 'Onboarding Status', 'Actions']}
+                                    >
+                                        {employeeData.map((emp, idx) => (
+                                            <tr key={idx}>
+                                                <td><Form.Check /></td>
+                                                <td><a href="#">{emp.id}</a></td>
+                                                <td>{emp.avatar} {emp.name}</td>
+                                                <td>{emp.department}</td>
+                                                <td>
+                                                    <span className="badge">
+                                                        <i></i> {emp.status}
+                                                    </span>
+                                                </td>
+                                                <td>{emp.email}</td>
+                                                <td><OnboardingBadge status={emp.onboarding} /></td>
+                                                <td className='table_action'>
+                                                    <Button className="btn_action"><img src={Images.Edit} alt="" /></Button>
+                                                    <Button className="btn_action"><img src={Images.Delete} alt="" /></Button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </PrimaryGird>
+                                </Col>
+                            </Tab>
+                            <Tab eventKey="new_onboarding" title="New Onboarding">
+                                2
+                            </Tab>
+                            <Tab eventKey="pending_onboarding" title="Pending Onboarding">
+                                3
+                            </Tab>
+                        </Tabs>
                     </Col>
                 </Row>
             </Container>
